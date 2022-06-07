d55abd117fcae2b64916439c5c847b65cee72e1b
Jared Good <jared_danell@yahoo.com>
2022-06-07 09:56:37 -0600

    fixed the JokeService Implementation. Closes #1

--- src/main/java/com/jaredgood/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/com/jaredgood/spring5jokesappv2/services/JokeServiceImpl.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(){
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
     @Override
     public String getJoke() {
-        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
         return chuckNorrisQuotes.getRandomQuote();
     }
 }
